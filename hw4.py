f0a6af75a3c140d5ced09f6153c97fbfa4fdb88c
Kelby Kies <kelbykies@Kelbys-MacBook-Pro.local>
2020-04-04 19:50:16 -0500

    We think we have alpha?

--- hw4.py
+++ hw4.py
@@ -42,6 +42,54 @@ def generate_hstring(h_list, set_char, prefix, set_len, str_len):
 
     return h_list
 
+def alpha_func(initial_n, n, eta0, eta1, pi, gamma, reads, qualities, tau, h):
+
+    alpha_vec = np.zeros(len(reads))
+    nuc = ['A', 'C', 'G', 'T']
+
+    list_of_hstrings = []
+    sample_space = generate_hstring(list_of_hstrings, nuc, "", len(nuc), h)
+
+
+
+
+    for i in range(0, len(reads)):
+
+        q = qualities[i]
+        r = reads[i]
+        alpha = 1
+        if r[0:h] == initial_n:
+            # Is this Pi(R,N) or Pir(N,R)???
+
+            for l in range(0, h):
+                alpha *= eta0[q[l]] * pi[code[initial_n[l]]][code[r[l]]] * gamma[code[initial_n[l]]]
+        else:
+            for l in range(0, h):
+                alpha *= eta1[q[l]] * pi[code[initial_n[l]]][code[r[l]]] * gamma[code[initial_n[l]]]
+
+        for j in range(1,len(reads[i])):
+            sum_thing = 0
+
+            if r[j] == n:
+
+                for k in nuc:
+                    sum_thing += tau[k][code[n]] * alpha
+
+                alpha = eta0[q[0]] * pi[code[n]][code[r[0]]] * sum_thing
+
+            else:
+
+                for k in nuc:
+                    sum_thing += tau[k][code[n]] * alpha
+
+                    alpha = eta1[q[0]] * pi[code[n]][code[r[0]]] * sum_thing
+        alpha_vec[i] = alpha
+
+    return alpha_vec
+
+
+
+
 
 
 
@@ -77,7 +125,7 @@ code={"A":0, "C":1, "G":2, "T":3 }
 
 nucs = ['A', 'C', 'G', 'T']
 # Order of markov chain
-h = 7
+h = 3
 
 # Import Fastq
 reads = []
@@ -87,7 +135,13 @@ tau = intialize_Tau(nucs, h)
 gamma = np.random.dirichlet(np.ones(pow(4,h)))
 eta0 = np.random.dirichlet(np.ones(40))
 eta1 = np.random.dirichlet(np.ones(40))
-pi = np.random.dirichlet(np.ones((4,4)),4)
+# print(eta1)
+
+pi = np.zeros((4,4))
+for i in range(0,4):
+    pi[i] = np.random.dirichlet(np.ones(4))
+
+
 for record in SeqIO.parse("test.fastq", "fastq"):
 
     reads.append(str(record.seq))
@@ -96,6 +150,9 @@ for record in SeqIO.parse("test.fastq", "fastq"):
 # Convert to Numpy Arrays
 reads = np.asarray(reads)
 qualities = np.asarray(qualities)
+
+print(len(reads))
+print(alpha_func('TTA','G', eta0, eta1, pi, gamma, reads, qualities, tau, h))
 
 
 
