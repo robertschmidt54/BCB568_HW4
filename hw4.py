725167d8284f2824e6abc91fe4ff183851f2e513
Kelby Kies <kelbykies@Kelbys-MacBook-Pro.local>
2020-04-06 22:04:37 -0500

    Alpha function with Dorman Updates 4/6

--- hw4.py
+++ hw4.py
@@ -44,71 +44,74 @@ def generate_hstring(h_list, set_char, prefix, set_len, str_len):
     return h_list
 
 
-def alpha_func(eta0, eta1, pi, gamma, reads, qualities, tau, h):
+def alpha_func(eta0, eta1, pi, gamma, read, quality, tau, h):
 
-    alpha_vec = np.ones((len(reads[0]), pow(4,h)))
+    alpha_vec = np.zeros((len(reads[0]), pow(4,h)))
     nuc = ['A', 'C', 'G', 'T']
     list_of_hstrings = []
     sample_space = generate_hstring(list_of_hstrings, nuc, "", len(nuc), h)
-
     position = 0
     j = 0
-    while j <= len(reads[0]):
-        # print(j)
-        if j > 0:
-            # Correct for Underflow
 
-            max_value = np.max(alpha_vec[position-1])
-            # alpha_vec[position - 1] = alpha_vec[position - 1] - max_value
-            alpha_vec[position-1] = np.exp(alpha_vec[position-1])
-            alpha_vec[position-1] = alpha_vec[position-1] - max_value
-            alpha_vec[position-1] = alpha_vec[position-1]/np.sum(alpha_vec[position-1])
-
-            if j == len(reads[0]):
-                break
+    # while j <= len(read):
+    #     # print(j)
+    #     if j > 0:
+    #         # Correct for Underflow
+    #
+    #         max_value = np.max(alpha_vec[position-1])
+    #         # alpha_vec[position - 1] = alpha_vec[position - 1] - max_value
+    #         alpha_vec[position-1] = np.exp(alpha_vec[position-1])
+    #         alpha_vec[position-1] = alpha_vec[position-1] - max_value
+    #         alpha_vec[position-1] = alpha_vec[position-1]/np.sum(alpha_vec[position-1])
+    #
+    #         if j == len(reads[0]):
+    #             break
         # print(j)
         # print(alpha_vec)
 
-        for s in sample_space:
-            alpha = 1
+    for s in sample_space:
+        alpha = 1
 
-            for i in range(0, len(reads)):
-                q = qualities[i]
-                r = reads[i]
-                sum_thing = 0
+        if read[0:h] == s:
+            # Assume the first H-mer is error free
+        # if j == 0:
+            for l in range(0, h):
 
-                if j == 0:
-                    for l in range(0,h):
-                        if r[l] == s[l]:
-                            alpha += math.log(eta0[q[l]-1] * pi[code[r[l]]][code[s[l]]] * gamma[code[s[l]]])
-                        else:
-                            alpha += math.log(eta1[q[l]-1] * pi[code[r[l]]][code[s[l]]] * gamma[code[s[l]]])
-                else:
-                    sum_thing = 0
-                    if r[j] == s[h-1]:
-                        n = s[h-1]
-                        sum_thing = tau.sum(axis=0)[n]
-                        # Something is wrong here.
-                        sum_thing *= alpha_vec[position-1][sample_space.index(s)]
-                        alpha += math.log(eta0[q[j]-1] * pi[code[r[j]]][code[n]] * sum_thing)
+                # Loop through first h-mer
 
-                    else:
-                        n = s[h - 1]
-                        sum_thing = tau.sum(axis=0)[n]
-                        sum_thing *= alpha_vec[position - 1][sample_space.index(s)]
-                        alpha += math.log(eta1[q[j]-1] * pi[code[r[j]]][code[n]] * sum_thing)
-                        # print(math.log(eta1[q[j]-1] * pi[code[r[j]]][code[n]] * sum_thing))
+                alpha_vec[0][sample_space.index(s)] += math.log(eta0[quality[l]-1] * pi[read[l]][s[l]] * gamma[code[s[l]]])
 
-                if i == len(reads)-1:
-                    alpha_vec[position][sample_space.index(s)] = alpha
-
-
-        if j == 0:
-            j += h
+                # alpha *= eta0[quality[l] - 1] * pi[code[read[l]]][code[s[l]]] * gamma[code[s[l]]]
         else:
-            j += 1
+            alpha_vec[0][sample_space.index(s)] = 0
 
-        position += 1
+        for j in range(h + 1, len(read)):
+            # for s in sample_space:
+            for s in nuc:
+                alpha_vec[j-h][sample_space.index(s)] = 0
+
+                for t in sample_space:
+                    alpha_vec[j - h][sample_space.index(s)] += math.exp(alpha_vec[j-h-1][sample_space.index(t)]) * tau[t][s]
+
+                    # alpha[j - h][s] += alpha[j - h - 1][t] * tau[t][s]
+
+            if read[j] == s[h-1]:
+
+                alpha_vec[j - h][sample_space.index(s)] = math.log(alpha_vec[j - h][sample_space.index(s)]) + math.log(eta0[quality[j]-1] * pi[read[j]][s[h-1]])
+                # n = s[h-1]
+                # sum_thing = tau.sum(axis=0)[n]
+                # # Something is wrong here.
+                # sum_thing *= alpha_vec[position-1][sample_space.index(s)]
+                # alpha += math.log(eta0[q[j]-1] * pi[code[r[j]]][code[n]] * sum_thing)
+
+            else:
+
+                alpha_vec[j - h][sample_space.index(s)] = math.log(alpha_vec[j - h][sample_space.index(s)]) + math.log(eta1[quality[j]-1] * pi[read[j]][s[h-1]])
+                # n = s[h - 1]
+                # sum_thing = tau.sum(axis=0)[n]
+                # sum_thing *= alpha_vec[position - 1][sample_space.index(s)]
+                # alpha += math.log(eta1[q[j]-1] * pi[code[r[j]]][code[n]] * sum_thing)
+                # print(math.log(eta1[q[j]-1] * pi[code[r[j]]][code[n]] * sum_thing))
 
     return alpha_vec
 
@@ -189,21 +192,18 @@ for record in SeqIO.parse("robs_test.fastq", "fastq"):
 reads = np.asarray(reads)
 qualities = np.asarray(qualities)
 
-h = 2
-# eta0 = np.zeros(40)
-# eta0[36:40] = [0.1, 0.1, 0.3, 0.4]
-#
-# eta1 = np.random.dirichlet(np.ones(40))
-# pi = np.array([[0.5, 0.25, 0.125, 0.125],[0.25, 0.25, 0.25, 0.25], [0.25, 0.25, 0.25, 0.25], [0.25, 0.25, 0.25, 0.25]])
-#
-# tau = intialize_Tau(nucs, h)
-# gamma = np.array([0.9,0.03,0.04,0.03])
+h = 1
+eta0 = np.zeros(40)
+eta0[36:40] = [0.1, 0.1, 0.3, 0.4]
 
-alpha = alpha_func(eta0, eta1, pi, gamma, reads, qualities, tau, h)
+eta1 = np.random.dirichlet(np.ones(40))
+pi = np.array([[0.5, 0.25, 0.125, 0.125],[0.25, 0.25, 0.25, 0.25], [0.25, 0.25, 0.25, 0.25], [0.25, 0.25, 0.25, 0.25]])
+pi = pd.DataFrame(pi, index= ('A', 'C', 'G', 'T'), columns= ('A', 'C', 'G', 'T'))
+
+tau = intialize_Tau(nucs, h)
+gamma = np.array([0.9,0.03,0.04,0.03])
+alpha = alpha_func(eta0, eta1, pi, gamma, reads[0], qualities[0], tau, h)
 print(alpha)
-
-
-
 
 
 # Export Data
