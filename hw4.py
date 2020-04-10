41cfd27cd24895532491c96feedc4d75fa00e868
Kelby Kies <kelbykies@Kelbys-MacBook-Pro.local>
2020-04-09 21:42:02 -0500

    Eijn and EIjzw working.

--- hw4.py
+++ hw4.py
@@ -111,14 +111,10 @@ def e_func(eta0, eta1, pi, gamma, read, quality, tau, h):
     list_of_hstrings = []
     sample_space = generate_hstring(list_of_hstrings, nuc, "", len(nuc), h)
     alpha = alpha_func(eta0, eta1, pi, gamma, read, quality, tau, h)
-    E_ijn = dict.fromkeys(['A', 'C', 'G', 'T'])
-    E_ijn['A']= []
-    E_ijn['C'] =[]
-    E_ijn['G']= []
-    E_ijn['T'] =[]
+    E_ijn = dict.fromkeys(sample_space, [])
 
     E_ijzw = np.zeros((pow(4, h), 4))
-
+    e = np.zeros((len(read), pow(4,h)))
     for j in range(len(read) - 1, -1, -1):
         # Calculate beta
 
@@ -137,17 +133,17 @@ def e_func(eta0, eta1, pi, gamma, read, quality, tau, h):
                         quality[j] - 1] * pi[t[h - 1]][read[j]] * tau[t[h - 1]][s]
 
         # Calculate the numerator of E_ijn for 1 read
-        e = np.multiply(alpha[j], beta_out[1])
-        E_ijn['A'].insert(0, e[0])
-        E_ijn['C'].insert(0, e[1])
-        E_ijn['G'].insert(0, e[2])
-        E_ijn['T'].insert(0, e[3])
+        e[j] = np.multiply(alpha[j], beta_out[1])
+
 
         # Calculate the numerator of E_ijzw for 1 read
         a = alpha[j-1]
         b = beta_out[1]
         E_ijzw += eijzw_num(a, b, pi, eta0, eta1, tau, read[j], quality[j], sample_space)
         beta_out[1] = beta_out[0]
+
+    for z in sample_space:
+        E_ijn[z] = e[:, sample_space.index(z)]
 
     eijn_sums = []
     for k in range(len(read)):
@@ -157,14 +153,23 @@ def e_func(eta0, eta1, pi, gamma, read, quality, tau, h):
 
         eijn_sums.append(position_sum)
 
-    # Dividng by the sum over all nucleotides within the same poisition.same read
-    E_ijn['A'] = [b / m for b, m in zip(E_ijn['A'], eijn_sums)]
-    E_ijn['C'] = [b / m for b, m in zip(E_ijn['C'], eijn_sums)]
-    E_ijn['G'] = [b / m for b, m in zip(E_ijn['G'], eijn_sums)]
-    E_ijn['T'] = [b / m for b, m in zip(E_ijn['T'], eijn_sums)]
 
+    # Dividing by the sum over all nucleotides within the same poisition same read
+    for z in sample_space:
+        E_ijn[z] = [b / m for b, m in zip(E_ijn[z], eijn_sums)]
 
     return E_ijn, E_ijzw
+
+
+def Update_Gamma(ei1n, n, h):
+
+    gamma_update = np.zeros(pow(4,h))
+
+    print(np.sum(ei1n)/n)
+
+
+    return gamma_update
+
 
 
 
@@ -191,27 +196,13 @@ def e_func(eta0, eta1, pi, gamma, read, quality, tau, h):
 
 
 code={"A":0, "C":1, "G":2, "T":3 }
-
 nucs = ['A', 'C', 'G', 'T']
-# Order of markov chain
-h = 2
 
 # Import Fastq
 reads = []
 qualities = []
-# Initial state matrix
-tau = intialize_Tau(nucs, h)
-gamma = np.random.dirichlet(np.ones(pow(4,h)))
-eta0 = np.random.dirichlet(np.ones(40))
-eta1 = np.random.dirichlet(np.ones(40))
 
-
-pi = np.zeros((4,4))
-for i in range(0,4):
-    pi[i] = np.random.dirichlet(np.ones(4))
-
-pi = pd.DataFrame(pi, index = ['A', 'C', 'G', 'T'], columns = ['A', 'C', 'G', 'T'])
-for record in SeqIO.parse("robs_test.fastq", "fastq"):
+for record in SeqIO.parse("test.fastq", "fastq"):
 
     reads.append(str(record.seq))
     qualities.append(record.letter_annotations["phred_quality"])
@@ -220,7 +211,24 @@ for record in SeqIO.parse("robs_test.fastq", "fastq"):
 reads = np.asarray(reads)
 qualities = np.asarray(qualities)
 
-# h = 1
+
+# Da Real stuff
+# Order of markov chain
+h = 1
+
+# Initial state matrix
+tau = intialize_Tau(nucs, h)
+gamma = np.random.dirichlet(np.ones(pow(4,h)))
+eta0 = np.random.dirichlet(np.ones(40))
+eta1 = np.random.dirichlet(np.ones(40))
+pi = np.zeros((4,4))
+list_of_hstrings = []
+sample_space = generate_hstring(list_of_hstrings, nucs, "", len(nucs), h)
+for i in range(0,4):
+    pi[i] = np.random.dirichlet(np.ones(4))
+pi = pd.DataFrame(pi, index = ['A', 'C', 'G', 'T'], columns = ['A', 'C', 'G', 'T'])
+
+# # Test Crap
 # eta0 = np.zeros(40)
 # eta0[36:40] = [0.1, 0.1, 0.3, 0.4]
 #
@@ -230,10 +238,27 @@ qualities = np.asarray(qualities)
 #
 # tau = pd.DataFrame(np.array([[0.25, 0.25, 0.25, 0.25], [0.25, 0.25, 0.25, 0.25], [0.25, 0.25, 0.25, 0.25], [0.25, 0.25, 0.25, 0.25]]), index=['A', 'C', 'G', 'T'], columns=['A', 'C', 'G', 'T'])
 # gamma = np.array([0.9,0.03,0.04,0.03])
-# alpha = alpha_func(eta0, eta1, pi, gamma, reads[0], qualities[0], tau, h)
-# # print(alpha)
-e_func(eta0, eta1, pi, gamma, reads[0], qualities[0], tau, h)
-# print(alpha)
+#
+#
+E_ijzw = np.zeros((pow(4, h),4))
+E_ijn = np.ones((pow(4,h), len(reads),len(reads[0])))
+
+for i in range(0,len(reads)):
+    temp_eijn = e_func(eta0, eta1, pi, gamma, reads[i], qualities[i], tau, h)[0]
+
+    E_ijzw += e_func(eta0, eta1, pi, gamma, reads[i], qualities[i], tau, h)[1]
+    # print(E_ijn)
+    for z in sample_space:
+        E_ijn[sample_space.index(z)][i] = temp_eijn[z]
+
+print(E_ijn)
+Update_Gamma(E_ijn[:,:,0], len(reads), h)
+# print(E_ijzw)
+
+
+
+
+
 
 
 # Export Data
